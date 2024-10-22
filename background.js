d4484bf0dbb0bda60adbd4ac4ece84bc1a6f57d3
ATMJin <atmjin@protonmail.com>
2024-10-22 21:57:22 +0800

    complete

--- background.js
+++ background.js
@@ -1,30 +1,28 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  const pageContent = message.content;
-
-  sendResponse(pageContent);
+  const question = message.content;
 
   // 呼叫 OpenAI API
-  //   fetch("https://api.openai.com/v1/completions", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Authorization: `Bearer YOUR_OPENAI_API_KEY`,
-  //     },
-  //     body: JSON.stringify({
-  //       model: "gpt-3.5-turbo", // 可以選擇不同的模型
-  //       prompt: pageContent,
-  //       max_tokens: 100,
-  //     }),
-  //   })
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log("OpenAI 回應:", data.choices[0].text);
-  //       sendResponse(data.choices[0].text);
-  //     })
-  //     .catch((error) => {
-  //       console.error("API 發送失敗:", error);
-  //       sendResponse("Error processing request");
-  //     });
+  fetch('https://api.openai.com/v1/chat/completions', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer `,
+    },
+    body: JSON.stringify({
+      model: "gpt-4o", // 可以選擇不同的模型
+      messages: [{ "role": "system", "content": "回答下面問題，不需要解釋，只要答案:" }, { "role": "user", "content": question }],
+      max_tokens: 100,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("OpenAI 回應:", data?.choices[0]?.message?.content);
+      sendResponse(data?.choices[0]?.message?.content);
+    })
+    .catch((error) => {
+      console.error("API 發送失敗:", error);
+      sendResponse("解答失敗");
+    });
 
   // 告知 Chrome 這是異步回應
   return true;
--- content.js
+++ content.js
@@ -1,7 +1,20 @@
 // 抓取頁面內容
-const pageContent = document.body.innerText || document.body.textContent;
-
-// 發送內容給 background.js 進行 API 處理
-chrome.runtime.sendMessage({ content: pageContent }, (response) => {
-  console.log("Response from OpenAI:", response);
+const startButton = document.querySelector('input[type="button"][value="開始作答"]');
+startButton?.addEventListener('click', () => {
+  setTimeout(() => {
+    console.log("document.body:", document.body);
+    const questForm = document.getElementById('responseForm');
+    const table = questForm.querySelector('table');
+    Array.from(table.querySelectorAll('tr')).slice(0, -1).forEach((el, index) => {
+      chrome.runtime.sendMessage({ content: el.innerText }, (response) => {
+        console.log("Response from OpenAI:", response);
+        // 假設 Q 是一個已經選擇的元素
+        var span = document.createElement('b'); // 創建一個新的 <span> 元素
+        span.textContent = response; // 設置 <span> 的文本內容
+        // 將 <span> 添加到 <td> 元素內
+        el.querySelectorAll('td')[1].appendChild(span);
+      });
+    });
+  }, 5000);
 });
+
